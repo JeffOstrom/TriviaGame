b3a7d0a3909fadf80bd5249899603a3542d1ff3e
Jeff Ostrom <j.ostrm@gmail.com>
2017-10-10 21:08:02 -0500

    js questions/html

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -3,3 +3,19 @@ var time = 30;
 var correct = 0;
 var incorrect = 0;
 var misses = 0; 
+
+// Variable using to create and array for the game questions 
+var questions = [
+	{   //List of specific questions to ask 
+		quest: "Chicago Blackhawks have won a total of 6 championships",
+		answer: "true"
+	},
+	{
+		quest: "Patrick Kane is the team captain",
+		answer: "false"
+	},
+	{
+		quest: "The home stadium for the Chicago Blackhawks is the United Center",
+		answer:
+	}
+];
